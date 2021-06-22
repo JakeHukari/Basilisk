aef46485b4393fd78348a49ed214dd6066fe0010
Krypt0c <58837465+Krypt0c@users.noreply.github.com>
2021-06-21 23:39:04 -0600

    Config

--- Basilisk.py
+++ Basilisk.py
@@ -11,3 +11,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+#Configuration#
+parser = OptionParser()
+now = datetime.datetime.now()
