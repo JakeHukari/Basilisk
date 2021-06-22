4c3b9100a84d84ec0aaf33dfee36407a5de9fa62
Krypt0c <58837465+Krypt0c@users.noreply.github.com>
2021-06-21 23:56:22 -0600

    Update Basilisk.py

--- Basilisk.py
+++ Basilisk.py
@@ -12,6 +12,21 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+#Graphics
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+   CWHITE  = '\33[37m'
+
+
 #Configuration
 parser = OptionParser()
 now = datetime.datetime.now()
